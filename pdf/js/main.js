804ed41d5d04d36634386410ed381723b269a85a
Danilo Marques <danilomarques20@hotmail.com>
2018-04-03 01:20:00 -0300

    Modificando para gerar arquivo word

--- pdf/js/main.js
+++ pdf/js/main.js
@@ -4,24 +4,27 @@
 		$("#gerarhtml").click(function(){
 			//recebendo o input
 			let palavra = $("#nome").val();
+			console.log(palavra);
 			//definindo o nome do arquivo
 			fileName = palavra+".pdf";
 
 			let html = "<h1>"+palavra+"</h1>"
 			//criando o html com os underscore para preencher
 			let arr = palavra.split("");
+			console.log(arr);
 			html += "<div id='genPdf'>";
 			for(let i = 0; i < arr.length; i++){
 				if(arr[i] !== " "){
 					arr[i] = "__ ";
+					html += "<p>"+arr.join("")+"</p>";
 				}
-				html += "<p>"+arr.join("")+"</p>";
 			}
 			//exibindo o botão para gerar pdf
 			$("#genPdf").show();
 			//enviando pro html
 			$("#pdfAqui").html(html)
 		});
+		/*
 		//gerando pdf a partir do html				
 		$("#genPdf").click(function(){
 			let doc = new jsPDF();
@@ -29,5 +32,14 @@
 				"width":500
 			});
 			doc.save(fileName);
-		});	
+		});	*/
+
+
+        $("#genPdf").click(function(event) {
+            $(document).googoose({
+                 area: '#pdfAqui'
+         });
+       
+    });
+
 	}); // fim do jquery
