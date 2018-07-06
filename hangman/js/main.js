11a4a4683e20967d1b5699c588af7c03e545be0f
Danilo Marques <danilomarques20@hotmail.com>
2018-07-05 22:18:42 -0300

    Adicionando a logica

--- hangman/js/main.js
+++ hangman/js/main.js
@@ -1,1 +1,82 @@
+// lista de nomes
 nomes = ['Abel', 'Abelardo', 'Ada', 'Adalberto', 'Adalgisa', 'Adela', 'Adelaide', 'Adelberto', 'Adèle', 'Adelina', 'Ademar', 'Adhemar', 'Adolfo', 'Adolpho', 'Adrian', 'Adriana', 'Adriane', 'Adrianne', 'Adriano', 'Adriene', 'Adrienne', 'Afonso', 'Ágata', 'Agatha', 'Agenor', 'Agnaldo', 'Agnes', 'Agostinho', 'Aguinaldo', 'Aiko', 'Airton', 'Ajit', 'Akahana', 'Akako', 'Alana', 'Alane', 'Alanna', 'Alanne', 'Alba', 'Alberta', 'Albertina', 'Alberto', 'Alceu', 'Alcides', 'Alcione', 'Alcyone', 'Alda', 'Aldaberto', 'Aldine', 'Aldo', 'Alec', 'Alecsandra', 'Alegra', 'Alejandra', 'Aleksandra', 'Alessandra', 'Alessandro', 'Alex', 'Alexandra', 'Alexandre', 'Alfonso', 'Alfredo', 'Alice', 'Alicia', 'Alisha', 'Allegra', 'Alonso', 'Álvaro', 'Alzira', 'Amadeu', 'Amadeus', 'Amanda', 'Amar', 'Amauri', 'Amaury', 'Amedeo', 'Amisha', 'Amita', 'Amiti', 'Amy', 'Ana', 'Anastasia', 'Andresa', 'Andressa', 'Andreza', 'Andrezza', 'Anete', 'Angel', 'Ângela', 'Angeli', 'Angelina', 'Angelita', 'Ângelo', 'Anita', 'Anna', 'Anne', 'Annete', 'Anoush', 'Anselmo', 'Antenor', 'Antonela', 'Antonella', 'Antônia', 'Antonieta', 'Antônio', 'Aparecida', 'Aquiles', 'Araci', 'Aracy', 'Areta', 'Aretha', 'Ariana', 'Ariane', 'Ariela', 'Ariella', 'Arielle', 'Arlene', 'Arlete', 'Armando', 'Arnaldo', 'Arthur', 'Artur', 'Asha', 'Astrid', 'Astride', 'Augusta', 'Augustina', 'Augustine', 'Augusto', 'Auro', 'Aurora', 'Auxiliadora', 'Ayrton', 'Ayumi', 'Babette', 'Balbina', 'Balraj', 'Baltazar', 'Barbie', 'Barbra', 'Bartolomeu', 'Beata', 'Beatrice', 'Beatrix', 'Beatriz', 'Bela', 'Belinda', 'Bella', 'Belle', 'Benedicta', 'Benedicto', 'Benedita', 'Benedito', 'Benito', 'Benjamim', 'Benjamin', 'Bento', 'Berenice', 'Bernadete', 'Bernadette', 'Bernarda', 'Bernardino', 'Bernardo', 'Berta', 'Bertha', 'Betânia', 'Bete', 'Beth', 'Betina', 'Bianca', 'Biatriz', 'Blanche', 'Branca', 'Brenda', 'Breno', 'Briana', 'Brianne', 'Bridget', 'Brigite', 'Brigitte', 'Brione', 'Bruna', 'Brunete', 'Bruno', 'Cacilda', 'Caetano', 'Caio', 'Calista', 'Calixta', 'Calixto', 'Camellia', 'Cameron', 'Camila', 'Camile', 'Camilla', 'Camille', 'Camilo', 'Candice', 'Cândida', 'Cândido', 'Capitu', 'Carina', 'Carine', 'Carla', 'Carlo', 'Carlos', 'Carlota', 'Carmel', 'Carmela', 'Carmelita', 'Carmem', 'Carmen', 'Carmina', 'Carmo', 'Carol', 'Carola', 'Carolina', 'Caroline', 'Carolyn', 'Carolyne', 'Cassandra', 'Cassiano', 'Cassilda', 'Catarina', 'Caterina', 'Catherine', 'Celeste', 'Celina', 'Celso', 'Ceres', 'Chandra', 'Charles', 'Charlotte', 'Chelsea', 'Chiara', 'Christal', 'Christian', 'Christiana', 'Christiane', 'Christina', 'Christopher', 'Chrystal', 'Cibele', 'Cilene', 'Cinthia', 'Ciro', 'Clair', 'Claire', 'Clara', 'Clarice', 'Clarissa', 'Clarisse', 'Claudete', 'Claudette', 'Cleide', 'Cleusa', 'Clotilda', 'Clotilde', 'Clotildes', 'Conrado', 'Consuelo', 'Cora', 'Corina', 'Cosette', 'Creusa', 'Creuza', 'Cristal', 'Cristiana', 'Cristiane', 'Cristiano', 'Cristina', 'Cynthia', 'Cyntia', 'Dafne', 'Dagmar', 'Dagmara', 'Daiana', 'Daiane', 'Daisy', 'Dalila', 'Dalton', 'Dalva', 'Dana', 'Daniel', 'Daniela', 'Daniele', 'Daniella', 'Danielle', 'Danilo', 'Dante', 'Daphne', 'Dara', 'Darci', 'Darcy', 'Daria', 'Dario', 'Darlene', 'Davi', 'David', 'Daya', 'Deborah', 'Deepak', 'Deise', 'Delfina', 'Demi', 'Denice', 'Dênis', 'Denise', 'Deva', 'Devi', 'Dhara', 'Diana', 'Diane', 'Diego', 'Dinah', 'Diogo', 'Dione', 'Dionise', 'Dipak', 'Dirce', 'Dirceu', 'Diva', 'Djalma', 'Djane', 'Dolores', 'Dominique', 'Donata', 'Dora', 'Doralice', 'Dorothy', 'Dulce', 'Dulcineia', 'Éder', 'Edgar', 'Édison', 'Edite', 'Edith', 'Edmundo', 'Edna', 'Édson', 'Eduardo', 'Elaine', 'Elba', 'Elenice', 'Eleonor', 'Eleonora', 'Eliana', 'Eliane', 'Elias', 'Élio', 'Elis', 'Elisa', 'Elisabete', 'Elisabeth', 'Eliseu', 'Eliza', 'Elizabete', 'Elizabeth', 'Eloah', 'Elsa', 'Elvira', 'Elvis', 'Elza', 'Elzira', 'Ema', 'Emanuel', 'Emanuela', 'Emanuele', 'Emanuelle', 'Émerson', 'Emily', 'Emma', 'Enrico', 'Enrique', 'Enzo', 'Erasmo', 'Eric', 'Érica', 'Érico', 'Érika', 'Ériko', 'Ernesto', 'Esmeralda', 'Estela', 'Ester', 'Esther', 'Eugênia', 'Eugênio', 'Eunice', 'Eva', 'Evandro', 'Evangelina', 'Eve', 'Évelin', 'Evelina', 'Eveline', 'Evelyn', 'Fabiana', 'Fabiano', 'Fabrizio', 'Fanny', 'Fausta', 'Faustina', 'Fausto', 'Felipa', 'Felipe', 'Ferdinando', 'Fernanda', 'Fernando', 'Filipa', 'Filipe', 'Filippo', 'Filomena', 'Fiona', 'Flor', 'Flora', 'Franca', 'Frances', 'Francesca', 'Francesco', 'Francine', 'Francis', 'Francisca', 'Francisco', 'Frederico', 'Frida', 'Gabriel', 'Gabriela', 'Gabriele', 'Gabriella', 'Gabrielle', 'Gaetano', 'Ganesh', 'Genji', 'George', 'Georgia', 'Georgiana', 'Georgina', 'Geralda', 'Geraldo', 'Germano', 'Gertrude', 'Gertrudes', 'Giancarlo', 'Gilberto', 'Gilda', 'Gilmar', 'Gilmara', 'Gilson', 'Gina', 'Gioconda', 'Giorgio', 'Giovana', 'Giovanna', 'Giovanni', 'Gisela', 'Giselda', 'Gisele', 'Gisella', 'Giselle', 'Gita', 'Giulia', 'Gizelda', 'Gladys', 'Glauce', 'Glauco', 'Glenda', 'Gonzalo', 'Grace', 'Graziela', 'Greice', 'Greta', 'Gretchen', 'Guilherme', 'Guiomar', 'Gunther', 'Gustavo', 'Gyselle', 'Haidê', 'Halima', 'Hamilton', 'Hannah', 'Haydê', 'Hebe', 'Hector', 'Heidi', 'Heitor', 'Helena', 'Helenice', 'Helga', 'Henrique', 'Henriqueta', 'Henry', 'Herculano', 'Hilda', 'Hildegard', 'Homero', 'Horishi', 'Hortênsia', 'Hugo', 'Humberto', 'Iara', 'Ícaro', 'Idalina', 'Ieda', 'Igor', 'Ilsa', 'Indra', 'Inês', 'Inez', 'Ingrid', 'Íngride', 'Iolanda', 'Ione', 'Iracema', 'Irene', 'Irina', 'Íris', 'Isa', 'Isaac', 'Isabel', 'Isabela', 'Isabele', 'Isabella', 'Isabelle', 'Isadora', 'Isaura', 'Isidora', 'Isidoro', 'Ísis', 'Ismael', 'Israel', 'Ítalo', 'Itamar', 'Iuri', 'Ivan', 'Ivete', 'Ivette', 'Ivo', 'Ivone', 'Ivonne', 'Izabela', 'Jaci', 'Jacira', 'Jacob', 'Jacqueline', 'Jacques', 'Jacy', 'Jacyra', 'Jade', 'Jaime', 'Jair', 'Jairo', 'Jamal', 'Jamil', 'Jamila', 'Jandir', 'Jandira', 'Jandyr', 'Jandyra', 'Jane', 'Janete', 'Janice', 'Jaqueline', 'Jasmim', 'Jasmin', 'Jasmina', 'Jasmine', 'Jean', 'Jefferson', 'Jeni', 'Jenifer', 'Jennifer', 'Jenny', 'Jeremias', 'Jin', 'Joana', 'Joanna', 'Joaquim', 'Joaquina', 'Joel', 'Joelle', 'Jonas', 'Jonatan', 'Jônatas', 'Jonathan', 'Jordana', 'Jorge', 'Josefina', 'Josephine', 'Josias', 'Joy', 'Juarez', 'Judite', 'Judith', 'Juliana', 'Juliano', 'Julieta', 'Jussara', 'Justina', 'Justino', 'Kaila', 'Kaio', 'Kalil', 'Kalila', 'Kaori', 'Karen', 'Karim', 'Karina', 'Karine', 'Karla', 'Karoline', 'Kate', 'Katerine', 'Katharina', 'Katherine', 'Katya', 'Keiko', 'Keila', 'Keith', 'Kelly', 'Kelvin', 'Khalil', 'Kim', 'Kin', 'Kristal', 'Kyoko', 'Laerte', 'Laila', 'Lailah', 'Lana', 'Lara', 'Larisa', 'Larissa', 'Lateefah', 'Latifa', 'Latiffa', 'Laura', 'Lauro', 'Layla', 'Leandra', 'Leandro', 'Leda', 'Leila', 'Leilah', 'Lenora', 'Leon', 'Leona', 'Leonardo', 'Leônidas', 'Leonor', 'Leonora', 'Leopoldo', 'Letizia', 'Li', 'Lia', 'Lien', 'Lila', 'Lilian', 'Liliana', 'Liliane', 'Lina', 'Linda', 'Lindsay', 'Linete', 'Linette', 'Lineu', 'Lisa', 'Lisandra', 'Lisandro', 'Liza', 'Lizandra', 'Lizandro', 'Lola', 'Lorelei', 'Lorena', 'Lorenzo', 'Loreta', 'Lourdes', 'Luana', 'Luca', 'Lucas', 'Luci', 'Luciana', 'Luciane', 'Luciano', 'Luciene', 'Lucila', 'Lucy', 'Ludemila', 'Ludmila', 'Luigi', 'Luiz', 'Luiza', 'Luna', 'Lurdes', 'Luzia', 'Lydia', 'Lynn', 'Mabel', 'Madalena', 'Mafalda', 'Magali', 'Magda', 'Magdalena', 'Magno', 'Maia', 'Maiara', 'Maitê', 'Malika', 'Manoel', 'Manoela', 'Manu', 'Manuel', 'Manuela', 'Manuelle', 'Mara', 'Marcel', 'Marcela', 'Marcele', 'Marcella', 'Marcello', 'Marcelo', 'Marco', 'Marcos', 'Margarida', 'Margarita', 'Margot', 'Maria', 'Mariah', 'Mariana', 'Mariângela', 'Marianne', 'Mariano', 'Marina', 'Marisa', 'Marise', 'Mariza', 'Marize', 'Marjorie', 'Marlene', 'Marli', 'Marly', 'Marta', 'Martha', 'Martim', 'Martina', 'Marvin', 'Mary', 'Masculino', 'Mateus', 'Matheus', 'Mathias', 'Matias', 'Matilda', 'Matilde', 'Maura', 'Mauro', 'Maya', 'Mayara', 'Mayra', 'Maysa', 'Megan', 'Meire', 'Melinda', 'Melissa', 'Melvin', 'Messias', 'Micael', 'Michaela', 'Michaella', 'Michel', 'Michele', 'Michelle', 'Midori', 'Miguel', 'Mika', 'Mildred', 'Milena', 'Milton', 'Mingmei', 'Miranda', 'Miriam', 'Mirna', 'Miyoko', 'Moema', 'Moira', 'Mônica', 'Monique', 'Mozart', 'Muriel', 'Murilo', 'Nadine', 'Nadir', 'Nadya', 'Naila', 'Nailah', 'Nair', 'Najma', 'Nancy', 'Naomi', 'Nara', 'Narcisa', 'Narciso', 'Natacha', 'Natalie', 'Natanael', 'Nataniel', 'Natasha', 'Nathalia', 'Nathanael', 'Nayana', 'Nei', 'Neide', 'Nereu', 'Nestor', 'Neusa', 'Ney', 'Nice', 'Nicholas', 'Nicolas', 'Nicolau', 'Nicole', 'Nika', 'Nilse', 'Nilton', 'Nilza', 'Nina', 'Nisha', 'Noel', 'Noemi', 'Noêmia', 'Nora', 'Norah', 'Norberto', 'Norma', 'Nuno', 'Odete', 'Odette', 'Odila', 'Olavo', 'Olga', 'Oliver', 'Omar', 'Onofre', 'Ophelia', 'Orfeu', 'Orlando', 'Oscar', 'Osmar', 'Osvaldo', 'Oswaldo', 'Oto', 'Otto', 'Pablo', 'Paco', 'Paloma', 'Pamela', 'Paola', 'Paolo', 'Pascoal', 'Pasqual', 'Patrick', 'Paula', 'Paulina', 'Paulo', 'Pedro', 'Perla', 'Peter', 'Phillip', 'Phillippe', 'Pierre', 'Pietro', 'Pilar', 'Priscila', 'Priscilla', 'Quincas', 'Rachel', 'Rafael', 'Rafaela', 'Rafaella', 'Raimundo', 'Raj', 'Rajesh', 'Rani', 'Raphael', 'Raquel', 'Raul', 'Rebeca', 'Rebecca', 'Regiane', 'Regina', 'Reinaldo', 'Renan', 'Renata', 'Renato', 'Reynaldo', 'Ricardo', 'Richard', 'Rita', 'Roberta', 'Roberto', 'Robinson', 'Robson', 'Rodolfo', 'Rodrigo', 'Roger', 'Rolando', 'Romeu', 'Rômulo', 'Rosa', 'Rosana', 'Rosane', 'Rose', 'Roxana', 'Roxane', 'Rubem', 'Rubens', 'Rui', 'Rute', 'Ruth', 'Ruy', 'Sabrina', 'Sacha', 'Safira', 'Salim', 'Salma', 'Salvador', 'Samanta', 'Samantha', 'Samara', 'Samir', 'Samira', 'Samuel', 'Sandra', 'Sandro', 'Sara', 'Sarah', 'Sasha', 'Saulo', 'Scarlet', 'Selene', 'Selma', 'Serafim', 'Serafina', 'Serena', 'Severino', 'Severo', 'Shakira', 'Sharon', 'Sheila', 'Sheilla', 'Shen', 'Shirlei', 'Shirley', 'Sibele', 'Sidnei', 'Sidney', 'Silena', 'Silvana', 'Simone', 'Socorro', 'Sofia', 'Solange', 'Sônia', 'Sophia', 'Sophie', 'Soraia', 'Soraya', 'Stefano', 'Stella', 'Sueli', 'Suely', 'Susana', 'Syon', 'Tabita', 'Taciana', 'Tadeu', 'Takashi', 'Tales', 'Talita', 'Tamara', 'Tancredo', 'Tânia', 'Tarsila', 'Tarso', 'Tassiana', 'Tatiana', 'Tatiane', 'Ted', 'Telma', 'Telmo', 'Teo', 'Teodora', 'Teodoro', 'Teresa', 'Thales', 'Thalita', 'Thelma', 'Theo', 'Theodoro', 'Theresa', 'Thiago', 'Thomas', 'Thomaz', 'Tiago', 'Tito', 'Tobias', 'Tomaz', 'Trudy', 'Ubaldo', 'Ugo', 'Ulisses', 'Úlrica', 'Ulrika', 'Umberto', 'Urbano', 'Úrsula', 'Vagner', 'Valdemar', 'Valdemir', 'Valdir', 'Valdo', 'Valdomiro', 'Valentim', 'Valentina', 'Valentino', 'Valmir', 'Valter', 'Vanda', 'Vanderlei', 'Vanderley', 'Vando', 'Vanessa', 'Vânia', 'Vera', 'Veridiana', 'Verônica', 'Vicente', 'Victor', 'Victorio', 'Vidal', 'Vilma', 'Violeta', 'Vivian', 'Viviana', 'Viviane', 'Vivien', 'Vladimir', 'Wagner', 'Waldemar', 'Waldemir', 'Waldemiro', 'Waldir', 'Waldo', 'Waldomiro', 'Waldyr', 'Wallace', 'Walmir', 'Walter', 'Wanda', 'Wanderley', 'Wando', 'Wanessa', 'Wendy', 'Wesley', 'William', 'Wilma', 'Wilson', 'Wilton', 'Wladimir', 'Xavier', 'Xaviera', 'Yasmim', 'Yasmin', 'Yasmina', 'Yasmine', 'Yoko', 'Yolanda', 'Yone', 'Yoshi', 'Yukio', 'Yumi', 'Yuri', 'Yvette', 'Yvone', 'Zacarias', 'Zara', 'Zenaide', 'Zenon', 'Zilda', 'Zoe', 'Zuleica', 'Zuleika', 'Zulmira', 'Abilio', 'Adelia', 'Aida', 'Ailton', 'Aimee', 'Alaide', 'Alexis', 'Aloisio', 'Aluisio', 'Amalia', 'Amelia', 'Amelie', 'America', 'Americo', 'Amilcar', 'Anais', 'Anastacia', 'Andre', 'Andrea', 'Andreia', 'Angelica', 'Anibal', 'Anisio', 'Ataulfo', 'Aurelia', 'Aurelio', 'Barbara', 'Basilio', 'Benicio', 'Boris', 'Brigida', 'Camelia', 'Cassia', 'Cassio', 'Cecile', 'Cecilia', 'Celia', 'Celine', 'Celio', 'Cesar', 'Chloe', 'Cicero', 'Cintia', 'Claudia', 'Claudio', 'Clelia', 'Cloe', 'Cordelia', 'Cornelia', 'Dalia', 'Debora', 'Decio', 'Delia', 'Desiree', 'Dina', 'Doris', 'Dorotea', 'Doroteia', 'Dulcinea', 'Eloa', 'Eloisa', 'Emilia', 'Emilio', 'Estefano', 'Fabia', 'Fabio', 'Fabiola', 'Fabricia', 'Fabricio', 'Fatima', 'Felicia', 'Felicio', 'Felix', 'Flavia', 'Flavio', 'Gerson', 'Gervasio', 'Gladis', 'Glauber', 'Glaucia', 'Gloria', 'Gregorio', 'Gualter', 'Hadrian', 'Haidee', 'Helio', 'Heloisa', 'Horacio', 'Iemanja', 'Ignacio', 'Inacio', 'Iona', 'Isaias', 'Jaco', 'Janaina', 'Jessica', 'Jose', 'Julia', 'Julio', 'Junior', 'Kassia', 'Katia', 'Kesia', 'Laercio', 'Lais', 'Laisa', 'Lavinia', 'Lazaro', 'Lea', 'Leia', 'Leticia', 'Lidia', 'Ligia', 'Lilia', 'Livia', 'Livio', 'Lucia', 'Lucio', 'Luis', 'Luisa', 'Maira', 'Maisa', 'Maome', 'Marcia', 'Marcio', 'Marilia', 'Mario', 'Mauricio', 'Mirian', 'Moises', 'Moyses', 'Nadia', 'Natalia', 'Nazare', 'Nelson', 'Nilson', 'Nivea', 'Nivia', 'Norton', 'Octavia', 'Octavio', 'Ofelia', 'Olimpia', 'Olimpio', 'Olivia', 'Olivio', 'Otavia', 'Otavio', 'Patricia', 'Patricio', 'Penelope', 'Plinio', 'Quiteria', 'Raisa', 'Raissa', 'Renee', 'Rogeria', 'Rogerio', 'Rosario', 'Salome', 'Salvio', 'Sergio', 'Silvia', 'Silvio', 'Tabata', 'Tais', 'Tarcisio', 'Thais', 'Tomas', 'Tome', 'Valeria', 'Valerio', 'Valquiria', 'Vinicio', 'Vinicius', 'Virginia', 'Vitor', 'Vitoria', 'Vitorio', 'Walquiria', 'Yona', 'Zelia']
+
+
+function chooseName(nomes){
+	/*
+	nomes: um array com um nome por elemento
+	retorna: um nome aleatorio
+	*/
+	maximo = nomes.length - 1
+	return nomes[Math.ceil(Math.random() * maximo)]
+}
+
+function is_in(str,arr){
+	/*
+	str: string/char 
+	arr: array
+	retorna: true caso o char/string esteja dentro do array
+	*/
+	for(let i = 0; i < arr.length; i++){
+		if(str == arr[i]){
+			return true
+		}
+	}
+	return false
+
+}
+
+function palavrasJaDitas(lettersGuessed, secretWord){
+	/*
+	lettersGuessed: um array com todas as letras ja arriscadas
+	secretWord: o nome no qual queremos descobrir
+	retorna: uma string com underscore para letras ainda não arriscadas
+	*/
+	let temp = []
+	for(let i = 0; i < secretWord.length; i++){
+		if(is_in(secretWord[i], lettersGuessed) == false){
+			temp.push("_ ")
+		}else{
+			temp.push(secretWord[i])
+		}
+	}
+	return temp.join("")
+}
+
+function isWordGuessed(lettersGuessed, secretWord){
+	/* 
+	lettersGuessed: um array com todas as letras ja arriscadas
+	secretWord: o nome secreto
+	retorna true caso todas as letras de secretWord ja tiverem sido arriscadas
+	*/
+	for(let i = 0; i < secretWord.length; i++){
+		if(is_in(secretWord[i],lettersGuessed) == false){
+			return false
+		}
+	}
+	return true
+}
+
+//definindo variaveis padrões
+//letras que ja foram arriscadas
+let lettersGuessed = []
+letras_disponiveis = "abcdefghijklmnopqrstuvwxyz"
+//nome para descobrir
+let secretWord = chooseName(nomes)
+console.log(secretWord)
+// o que ainda falta descobrir
+document.getElementById("secretWord").innerHTML = palavrasJaDitas(lettersGuessed,secretWord)
+document.getElementById("letras_disponiveis").innerHTML = letras_disponiveis
+
+//evento do botão
+document.getElementById("arriscar").addEventListener("click",function(){
+	input = document.getElementById("input_user").value
+	if(is_in(input,lettersGuessed) == false){
+		lettersGuessed.push(input)
+	}
+	if(isWordGuessed(lettersGuessed, secretWord)){
+		alert("Você venceu!")
+	}
+	document.getElementById("secretWord").innerHTML = palavrasJaDitas(lettersGuessed,secretWord)
+
+});
